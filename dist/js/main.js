d3b84cffc9cc5632d33f7d21e3fa321b96c9af69
Alexey_Lisenko <detoner777@gmail.com>
2019-09-21 12:11:18 +0300

    About page done

--- dist/js/main.js
+++ dist/js/main.js
@@ -30,3 +30,67 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+//Урок
+console.log("Request data...");
+
+// setTimeout(() => {
+//   console.log("Preparing data...");
+//   const backendData = {
+//     server: "aws",
+//     port: 2000,
+//     status: "working"
+//   };
+//   setTimeout(() => {
+//     backendData.modifed = true;
+//     console.log("Data received", backendData);
+//   }, 2000);
+// }, 2000);
+
+// const p = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("Preparing data...");
+//     const backendData = {
+//       server: "aws",
+//       port: 2000,
+//       status: "working"
+//     };
+//     resolve(backendData);
+//   }, 2000);
+// });
+
+// p.then(data => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       data.modified = true;
+//       resolve(data);
+//     }, 2000);
+//   });
+// })
+
+//   .then(clientData => {
+//     clientData.fromPromise = true;
+//     return clientData;
+//   })
+//   .then(data => {
+//     console.log("Modified", data);
+//   })
+//   .catch(err => console.error("Error", err))
+//   .finally(() => console.log("finally"));
+
+const sleep = ms => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), ms);
+  });
+};
+
+// sleep(2000).then(() => console.log("After 2 sec"));
+// sleep(3000).then(() => console.log("After 3 sec"));
+
+Promise.all([sleep(2000), sleep(5000)]).then(() => {
+  console.log("All promises");
+});
+
+Promise.race([sleep(2000), sleep(5000)]).then(() => {
+  console.log("Race promises");
+});
